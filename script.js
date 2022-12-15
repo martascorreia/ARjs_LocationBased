a01cdc955aa7e60b06961bb5d84d2888bd90f60d
Marta Correia <48736172+martacorreia29@users.noreply.github.com>
2022-12-15 17:54:47 +0000

    again

--- script.js
+++ script.js
@@ -37,20 +37,20 @@ var models = [{
         url: 'https://martacorreia29.github.io/ARjs_LocationBased/assets/magnemite/scene.gltf',
         scale: '0.4 0.4 0.4',
         info: 'Magnemite, Lv. 5, HP 10/10',
-        rotation: '0 0 0',
+        rotation: '0 180 0',
         position: "0 0 -10"
     },
     {
         url: 'https://martacorreia29.github.io/ARjs_LocationBased/assets/articuno/scene.gltf',
         scale: '0.2 0.2 0.2',
-        rotation: '0 0 0',
+        rotation: '0 180 0',
         info: 'Articuno, Lv. 80, HP 100/100',
-        position: "0 -10 -50"
+        position: "0 -10 -20"
     },
     {
         url: 'https://martacorreia29.github.io/ARjs_LocationBased/assets/dragonite/scene.gltf',
         scale: '0.04 0.04 0.04',
-        rotation: '0 0 0',
+        rotation: '0 180 0',
         info: 'Dragonite, Lv. 99, HP 150/150',
         position: "0 0 -20"
     },
