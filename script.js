ce6751692d3bfa89fb28a4ff21578f315afb994b
Marta Correia <48736172+martacorreia29@users.noreply.github.com>
2022-12-15 16:18:58 +0000

    adjusting

--- script.js
+++ script.js
@@ -25,19 +25,22 @@ var models = [{
         url: './assets/magnemite/scene.gltf',
         scale: '0.5 0.5 0.5',
         info: 'Magnemite, Lv. 5, HP 10/10',
-        rotation: '0 180 0'
+        rotation: '0 180 0',
+        position: "0 0 -20"
     },
     {
         url: './assets/articuno/scene.gltf',
         scale: '0.2 0.2 0.2',
         rotation: '0 180 0',
         info: 'Articuno, Lv. 80, HP 100/100',
+        position: "0 0 -20"
     },
     {
         url: './assets/dragonite/scene.gltf',
         scale: '0.08 0.08 0.08',
         rotation: '0 180 0',
         info: 'Dragonite, Lv. 99, HP 150/150',
+        position: "0 0 -20"
     },
 ];
 
@@ -67,6 +70,7 @@ function renderPlaces(places) {
     places.forEach((place) => {
         let latitude = place.location.lat;
         let longitude = place.location.lng;
+        console.log(latitude + " " + longitude)
 
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
