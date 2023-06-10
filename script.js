679680f704dfe95e3978a40bec2e16efba2db54e
Marta Correia <marta.b.correia@accenture.com>
2023-06-10 17:20:03 +0100

    fix

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 window.onload = () => {
+    navigator.geolocation.getCurrentPosition(userLocation);
+};
+
+function userLocation(position) {
     let userLat = position.coords.latitude;
     let userLng = position.coords.longitude;
     console.log(userLat + " " + userLng);
-};
+}
