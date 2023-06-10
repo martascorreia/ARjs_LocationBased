3e35aa01191237551cfc64f09d3449c7785183da
Marta Correia <marta.b.correia@accenture.com>
2023-06-10 19:40:46 +0100

    new components

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+import * as THREE from 'three';
+import * as THREEx from './node_modules/@ar-js-org/ar.js/three.js/build/ar-threex-location-only.js';
+
 window.onload = () => {
     navigator.geolocation.getCurrentPosition(userLocation);
 };
